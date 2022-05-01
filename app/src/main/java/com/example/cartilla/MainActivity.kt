22ec8d9c265490b890d16695097e5e5ca413633a
Octavio Resendiz Bolaños <octavioresendiz36@aragon.unam.mx>
2022-04-30 20:48:22 -0500

    Los iconos de la navegacion ya tienen accion

--- app/src/main/java/com/example/cartilla/MainActivity.kt
+++ app/src/main/java/com/example/cartilla/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.cartilla
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation:BottomNavigationView = findViewById(R.id.botton_nav)
+
+        bottomNavigation.setOnItemSelectedListener {
+            item ->
+            when(item.itemId){
+                R.id.home ->{
+                    return@setOnItemSelectedListener true
+                }
+                R.id.profile ->{
+                    return@setOnItemSelectedListener true
+                }
+                R.id.shot ->{
+                    return@setOnItemSelectedListener true
+                }
+            }
+        }
     }
 }
