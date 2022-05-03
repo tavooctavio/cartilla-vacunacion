9379f042820e62326f9de39c984ccea75dcce16a
Octavio Resendiz Bolaños <octavioresendiz36@aragon.unam.mx>
2022-05-02 19:48:35 -0500

    Cree y enlace los fragment con sus botones para que cambie dependinedo de la opcion que se presiona

--- app/src/main/java/com/example/cartilla/MainActivity.kt
+++ app/src/main/java/com/example/cartilla/MainActivity.kt
@@ -2,28 +2,55 @@ package com.example.cartilla
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        lateinit var toolbar: ActionBar
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        toolbar = supportActionBar!!
+
         val bottomNavigation:BottomNavigationView = findViewById(R.id.botton_nav)
+
+        toolbar.title = "Home"
+        val homeFragment = Home.newInstance()
+        changeFragmnet(homeFragment)
 
         bottomNavigation.setOnItemSelectedListener {
             item ->
             when(item.itemId){
                 R.id.home ->{
+                    toolbar.title = "Home"
+                    val homeFragment = Home.newInstance()
+                    changeFragmnet(homeFragment)
                     return@setOnItemSelectedListener true
                 }
                 R.id.profile ->{
+                    toolbar.title = "Profile"
+                    val profileFragment = Profile.newInstance()
+                    changeFragmnet(profileFragment)
                     return@setOnItemSelectedListener true
                 }
                 R.id.shot ->{
+                    toolbar.title = "Shote"
+                    val homeFragment = Shot.newInstance()
+                    changeFragmnet(homeFragment)
                     return@setOnItemSelectedListener true
                 }
             }
+            false
         }
+    }
+
+    private fun changeFragmnet(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.commit()
     }
 }
